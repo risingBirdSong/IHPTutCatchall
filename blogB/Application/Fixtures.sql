1b32aefc27b4b379817f049ffe63d458223a303b
peter <peter@Susans-MacBook-Air-2.local>
2021-04-04 19:59:06 -0700

    completed again without peeking, getting much more comfortable and familiar. also learned that a deleteroute must include js-delete or it wont work

--- blogB/Application/Fixtures.sql
+++ blogB/Application/Fixtures.sql
@@ -12,3 +12,23 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 
+SET SESSION AUTHORIZATION DEFAULT;
+
+ALTER TABLE public.posts DISABLE TRIGGER ALL;
+
+INSERT INTO public.posts (id, title, body, created_at) VALUES ('f0a33dbc-f924-466b-a93e-2a2e0f30d0ff', 'posting!!!  :)', 'by me !!', '2021-04-04 15:57:56.483348-07');
+INSERT INTO public.posts (id, title, body, created_at) VALUES ('79e9e32b-0323-4878-8380-619eb2df35fe', 'AAA', 'aaa', '2021-04-04 19:16:41.046884-07');
+INSERT INTO public.posts (id, title, body, created_at) VALUES ('baf12315-e2ba-412e-9a9b-00ef3c850ec9', 'BBB', 'bbbbbb', '2021-04-04 19:16:48.28883-07');
+INSERT INTO public.posts (id, title, body, created_at) VALUES ('76c1eb61-6834-499a-9db9-51c83afb7c61', 'CCC', 'ccc', '2021-04-04 19:16:54.616339-07');
+
+
+ALTER TABLE public.posts ENABLE TRIGGER ALL;
+
+
+ALTER TABLE public.comments DISABLE TRIGGER ALL;
+
+
+
+ALTER TABLE public.comments ENABLE TRIGGER ALL;
+
+
--- blogB/Web/View/Posts/Show.hs
+++ blogB/Web/View/Posts/Show.hs
@@ -1,7 +1,7 @@
 module Web.View.Posts.Show where
 import Web.View.Prelude
 
-data ShowView = ShowView { post :: Post }
+data ShowView = ShowView { post :: Include "comments" Post }
 
 instance View ShowView where
     html ShowView { .. } = [hsx|
@@ -15,5 +15,14 @@ instance View ShowView where
         <p>{ get #body post}</p>
         <a href={EditPostAction (get #id post) }>edit post</a>
         <hr>
-        
+        <p>{get #createdAt post |> timeAgo}</p>
+        <a href={NewCommentAction (get #id post)}>add comment</a>
+        <h1>{forEach (get #comments post) renderComment }</h1>
     |]
+
+renderComment comment = [hsx|
+    <hr>
+    <h4>{get #author comment }</h4>
+    <p>{get #body comment}</p>
+    <p><a href={EditCommentAction (get #id comment)}>edit comment</a> ... <a href={DeleteCommentAction (get #id comment)} class="js-delete text-muted" > delete comment</a> </p>
+|]
